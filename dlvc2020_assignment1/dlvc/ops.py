3f4d8591ce2f54d02c06271a6c3fe3fb52756acc
tkauf15k <thomas@tkaufmann.at>
2020-04-11 16:01:24 +0200

    added impl for batch class and operations

--- dlvc2020_assignment1/dlvc/ops.py
+++ dlvc2020_assignment1/dlvc/ops.py
@@ -6,6 +6,7 @@ from typing import List, Callable
 # See https://docs.python.org/3/library/typing.html#typing.Callable for what this line means
 Op = Callable[[np.ndarray], np.ndarray]
 
+
 def chain(ops: List[Op]) -> Op:
     '''
     Chain a list of operations together.
@@ -18,38 +19,46 @@ def chain(ops: List[Op]) -> Op:
 
     return op
 
+
 def type_cast(dtype: np.dtype) -> Op:
     '''
     Cast numpy arrays to the given type.
     '''
 
-    # TODO implement (see above for guidance).
+    def op(sample: np.ndarray) -> np.ndarray:
+        return sample.astype(dtype=dtype)
 
-    pass
+    return op
+
 
 def vectorize() -> Op:
     '''
     Vectorize numpy arrays via "numpy.ravel()".
     '''
 
-    # TODO implement (see above for guidance).
+    def op(sample: np.ndarray) -> np.ndarray:
+        return np.ravel(sample)
 
-    pass
+    return op
+
 
 def add(val: float) -> Op:
     '''
     Add a scalar value to all array elements.
     '''
 
-    # TODO implement (see above for guidance).
+    def op(sample: np.ndarray) -> np.ndarray:
+        return np.add(sample, val)
 
-    pass
+    return op
+
 
 def mul(val: float) -> Op:
     '''
     Multiply all array elements by the given scalar.
     '''
 
-    # TODO implement (see above for guidance).
+    def op(sample: np.ndarray) -> np.ndarray:
+        return np.multiply(sample, val)
 
-    pass
+    return op
