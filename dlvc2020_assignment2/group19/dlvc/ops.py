2ab9c5b379ce92005671d2f9820540afb7fae849
dschoerk <dschoerk@gmx.at>
2020-05-23 11:29:40 +0200

    added part2 assignment code to our code

--- dlvc2020_assignment2/group19/dlvc/ops.py
+++ dlvc2020_assignment2/group19/dlvc/ops.py
@@ -62,3 +62,45 @@ def mul(val: float) -> Op:
         return np.multiply(sample, val)
 
     return op
+
+
+
+def hwc2chw() -> Op:
+    '''
+    Flip a 3D array with shape HWC to shape CHW.
+    '''
+
+    # TODO implement (see np.transpose)
+
+    pass
+
+def chw2hwc() -> Op:
+    '''
+    Flip a 3D array with shape CHW to HWC.
+    '''
+
+    # TODO implement (see np.transpose)
+
+    pass
+
+def hflip() -> Op:
+    '''
+    Flip arrays with shape HWC horizontally with a probability of 0.5.
+    '''
+
+    # TODO implement (numpy.flip will be helpful)
+
+    pass
+
+def rcrop(sz: int, pad: int, pad_mode: str) -> Op:
+    '''
+    Extract a square random crop of size sz from arrays with shape HWC.
+    If pad is > 0, the array is first padded by pad pixels along the top, left, bottom, and right.
+    How padding is done is governed by pad_mode, which should work exactly as the 'mode' argument of numpy.pad.
+    Raises ValueError if sz exceeds the array width/height after padding.
+    '''
+
+    # TODO implement
+    # https://numpy.org/doc/1.18/reference/generated/numpy.pad.html will be helpful
+
+    pass
