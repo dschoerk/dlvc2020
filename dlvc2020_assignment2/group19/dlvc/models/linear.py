e1daf873a1b6f8c1697abfa360352500e43a8bcf
dschoerk <dschoerk@gmx.at>
2020-05-23 11:20:36 +0200

    inital code for part2 from assignment 1; removed normalization of gradient according to feedback

---
+++ dlvc2020_assignment2/group19/dlvc/models/linear.py
@@ -0,0 +1,159 @@
+from ..model import Model
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+
+class LinearClassifier(Model):
+    '''
+    Linear classifier without bias.
+    Returns softmax class scores (see lecture slides).
+    '''
+
+    def __init__(self, input_dim: int, num_classes: int, lr: float, momentum: float, nesterov: bool):
+        '''
+        Ctor.
+        input_dim is the length of input vectors (> 0).
+        num_classes is the number of classes (> 1).
+        lr: learning rate to use for training (> 0).
+        momentum: momentum to use for training (> 0).
+        nesterov: training with or without Nesterov momentum.
+        '''
+
+        self.input_dim = input_dim
+        self.num_classes = num_classes
+        self.momentum = momentum
+        self.nesterov = nesterov
+        self.lr = lr
+
+        self.weights = torch.randn(input_dim, num_classes, requires_grad=True, dtype=torch.float)
+        self.v = torch.zeros(self.weights.size())
+
+        # TODO implement
+
+    def input_shape(self) -> tuple:
+        '''
+        Returns the expected input shape as a tuple, which is (0, input_dim).
+        '''
+
+        return (0, self.input_dim)
+
+    def output_shape(self) -> tuple:
+        '''
+        Returns the shape of predictions for a single sample as a tuple, which is (num_classes,).
+        '''
+
+        return (self.num_classes,)
+
+        # TODO implement
+
+    def train(self, data: np.ndarray, labels: np.ndarray) -> float:
+        '''
+        Train the model on batch of data.
+        Data are the input data, with shape (m, input_dim) and type np.float32 (m is arbitrary).
+        Labels has shape (m,) and integral values between 0 and num_classes - 1.
+        Returns the current cross-entropy loss on the batch.
+        Raises TypeError on invalid argument types.
+        Raises ValueError on invalid argument values.
+        Raises RuntimeError on other errors.
+        '''
+
+        if data.dtype != np.float32:
+            raise TypeError("invalid data datatype")
+
+        if labels.dtype != np.int and labels.dtype != np.uint:
+            raise TypeError("invalid label datatype")
+
+        if labels.shape[0] != data.shape[0] or data.shape[1] != self.input_shape()[1]:
+            raise TypeError("invalid input dimensions")
+
+        if (labels < 0).any() or (labels >= self.num_classes).any():
+            raise ValueError()
+
+        try:
+
+            # print(data.shape)
+            #print(labels.shape)
+
+            #print(data.shape)
+            # print(labels.shape)
+
+            loss = nn.CrossEntropyLoss()
+            input = torch.tensor(data, dtype=torch.float)
+
+            scores = torch.mm(input, self.weights) if not self.nesterov else torch.mm(input, self.weights + self.v)
+            # pred = scores.argmax(dim=0)
+            # print(pred)
+
+            # print(scores.shape)
+
+            labels = torch.tensor(labels, dtype=torch.long)
+            # print(labels)
+            # print(labels.shape)
+
+            # apply softmax to pred here?
+            output = loss(
+                scores,
+                labels)
+            output.backward()
+
+            # check with SGD from torch
+            # optimizer = torch.optim.SGD([self.weights], lr=0.1, momentum=0.9)
+            # optimizer.step()
+
+            grad = self.weights.grad
+            # print("gradient: %s" % str(grad))
+            # print("loss %f" % output.data)
+
+            # TODO implement (compute loss)
+
+            # self.weights.retain_grad() # include this tensor in the computation graph
+            # loss.backward() # compute gradients with backpropagation
+
+            # TODO implement (update weights with gradient descent)
+
+            # compute velocity
+            # grad = grad / grad.norm() # gradient normalization is incorrect
+            # print(grad)
+            self.v = self.v * self.momentum - grad * self.lr
+            # self.v = -grad
+
+            # update weights
+            self.weights.data = self.weights + self.v
+
+            return output.detach().numpy()
+
+        except Exception as e:
+            raise RuntimeError(str(e))
+
+    def predict(self, data: np.ndarray) -> np.ndarray:
+        '''
+        Predict softmax class scores from input data.
+        Data are the input data, with a shape compatible with input_shape().
+        The label array has shape (n, output_shape()) with n being the number of input samples.
+        Raises TypeError on invalid argument types.
+        Raises ValueError on invalid argument values.
+        Raises RuntimeError on other errors.
+        '''
+
+        if data.dtype != np.float32:
+            raise TypeError()
+
+        if data.shape[1] != self.input_shape()[1]:
+            raise TypeError("invalid input dimensions")
+
+        try:
+            input = torch.tensor(data, dtype=torch.float)
+            scores = torch.mm(input, self.weights)
+            # print(scores)
+            sm = nn.Softmax(dim=1)
+            scores = sm(scores)
+            r = scores.detach().numpy()
+            #print(r.shape)
+            
+            return r
+
+            # TODO implement
+        except Exception as e:
+            raise RuntimeError(str(e))
